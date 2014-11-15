350952b769b8c5463a30d6bd3e1b16b58bccaddf
sbagroy986 <shrey14099@iiitd.ac.in>
2014-11-15 20:38:57 +0530

    Preparing to deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+def after_sign_in_path_for(resource_or_scope)
+    if resource.sign_in_count == 1
+       view_dash_path
+    else
+       root_path
+    end
+end
 end
